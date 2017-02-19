ede5e888018fcafc6cf79a487d74f514fc3cfcef
Matt Longbrake <longbrmb@gmail.com>
2017-02-19 15:31:18 -0500

    Updated generate script to detect category typos.
    Script now generates a list of categories from the category tags on the
    restaurant entries and compares it to the supplied category list.  If
    they do not match it will throw an exception.

--- gen_matrix.py
+++ gen_matrix.py
@@ -2,20 +2,33 @@
 import json
 from jinja2 import FileSystemLoader, Environment
 
-# Load restaurant data from JSON
+# Load restaurant list from JSON
 with open('restaurant_list.json') as f:
     data = json.load(f)
 
+desired_cats = data['Categories']
+
 # Create a list of restaurants for each category
 lists = {}
-for cat in data['Categories']:
-    lists[cat] = [x['name'] for x in data['Restaurants'] 
-            if cat in x['categories']]
+found_cats = set()
+for rest in data['Restaurants']:
+    for cat in rest['categories']:
+        found_cats.add(cat)
+        try:
+            lists[cat].append(rest['name'])
+        except KeyError:
+            lists[cat] = [rest['name']]
+
+# Ensure that the categories found in the restaurant list match those in the
+# desired category list.  This is essentially a typo check on the JSON.
+if set(desired_cats) != found_cats:
+    raise ValueError('Detected restaurant categories do not match the '
+            'category list.')
 
 loader = FileSystemLoader('.')
 env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
 template = env.get_template('template.html')
-output = template.render(Categories=data['Categories'], lists=lists)
+output = template.render(categories=desired_cats, lists=lists)
 
 with open('mobile.html', 'w') as f:
     f.write(output)
